9c0527bb9ada25595aceb10fd61869a7996c018d
Bunny McIntyre <bunnymcintyre@Bunnys-MacBook-Pro.local>
2022-04-30 13:23:39 -0500

    added cross origin and rest controller in Home Controller

--- backend/src/main/java/controllers/HomeController.java
+++ backend/src/main/java/controllers/HomeController.java
@@ -1,2 +1,12 @@
-package controllers;public class HomeController {
+package controllers;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@RestController
+@RequestMapping("/api/project")
+public class HomeController {
 }
